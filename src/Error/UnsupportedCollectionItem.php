f4d2a5529829b579422e65cec57c32d978398da2
Fabio Ventura <fabio.ventura@facile.it>
2021-02-10 17:42:40 +0100

    #1 add type hinting

--- src/Error/UnsupportedCollectionItem.php
+++ src/Error/UnsupportedCollectionItem.php
@@ -6,8 +6,8 @@ namespace Cnastasi\DDD\Error;
 
 class UnsupportedCollectionItem extends TypeError
 {
-    public function __construct(string $unsupportedClass, $supportedClass)
+    public function __construct(string $unsupportedClass, string $supportedClass)
     {
-        parent::__construct("Unsupported item class $unsupportedClass, $supportedClass supported");
+        parent::__construct("Unsupported item class {$unsupportedClass}, {$supportedClass} supported");
     }
 }
