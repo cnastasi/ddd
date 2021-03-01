9912ca84a2da06b5c3b1292633d7b30915e051a5
Christian Nastasi <christian.nastasi@gmail.com>
2021-03-01 19:43:40 +0100

    Improved PaginableEntityCollection psalm annotations

--- src/Collection/PaginableEntityCollection.php
+++ src/Collection/PaginableEntityCollection.php
@@ -4,6 +4,14 @@ declare(strict_types=1);
 
 namespace CNastasi\DDD\Collection;
 
+use CNastasi\DDD\ValueObject\AbstractEntity;
+
+/**
+ * @template K of int|string
+ * @template T of AbstractEntity
+ *
+ * @extends EntityCollection<K, T>
+ */
 abstract class PaginableEntityCollection extends EntityCollection
 {
     private ?int $total = null;
