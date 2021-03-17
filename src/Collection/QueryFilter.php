a7506eaf883092487ab0b99f32be567cd85781ed
Christian Nastasi <christian.nastasi@gmail.com>
2021-03-17 12:15:49 +0100

    Introduction of Filter interface

---
+++ src/Collection/QueryFilter.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CNastasi\DDD\Collection;
+
+use CNastasi\DDD\Contract\Stringable;
+
+class QueryFilter implements Stringable
+{
+    private ?string $value;
+
+    public function __construct(?string $value)
+    {
+        $this->value = $value;
+    }
+
+    public function getValue(): ?string
+    {
+        return $this->value;
+    }
+
+    public function isSet(): bool
+    {
+        return $this->value !== null && $this->value !== '';
+    }
+
+    public function __toString(): string
+    {
+        return (string) $this->value;
+    }
+}
